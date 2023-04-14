c563ffb8c14268e1350a6e28afe91f9006398f05
Jmartin-GSS <45777727+Jmartin-GSS@users.noreply.github.com>
2023-04-14 12:26:17 -0500

    Final fixes. (Probably)

--- Globie/Form1.cs
+++ Globie/Form1.cs
@@ -12,6 +12,7 @@ namespace Globie
     public partial class f_main : Form
     {
         codeFile file = new codeFile();
+        string preppedTraining;
 
         public f_main()
         {
@@ -55,47 +56,6 @@ namespace Globie
         private async void askGlobie_ClickAsync(object sender, EventArgs e)
         {
 
-            if (tabControl1.SelectedIndex == 1)
-            {
-                l_Status.Text = "Training on AP + AR...";
-                l_Status.ForeColor = Color.Blue;
-                b_Send.Enabled = false;
-
-                string preppedTraining = Properties.Resources.AP_AddInvtoCCVend.ToString();
-
-                var chatGpt = new ChatGPT();
-
-                var completionResult = await chatGpt.OpenAIService.ChatCompletion.CreateCompletion
-                       (new ChatCompletionCreateRequest()
-                       {
-                           Messages = new List<ChatMessage>(new ChatMessage[]
-                                    { new ChatMessage("user", preppedTraining) }),
-                           Model = Models.ChatGpt3_5Turbo,
-                           Temperature = 0.4F,
-                           MaxTokens = 1000,
-                           N = 1
-                       });
-
-
-                preppedTraining = Properties.Resources.AR_WriteOffBadDebt_PDF.ToString();
-                completionResult = await chatGpt.OpenAIService.ChatCompletion.CreateCompletion
-                       (new ChatCompletionCreateRequest()
-                       {
-                           Messages = new List<ChatMessage>(new ChatMessage[]
-                                    { new ChatMessage("user", preppedTraining) }),
-                           Model = Models.ChatGpt3_5Turbo,
-                           Temperature = 0.4F,
-                           MaxTokens = 1000,
-                           N = 1
-                       });
-
-
-                rt_response.Text = "I'm trained and ready to answer your questions as accurately as possible!";
-                l_Status.Text = "Ready!";
-                l_Status.ForeColor = Color.Green;
-                b_Send.Enabled = true;
-
-            }
         }
 
         private void f_main_Load(object sender, EventArgs e)
@@ -167,12 +127,85 @@ namespace Globie
                 {
                     System.Threading.Thread.Sleep(2000);
                     chatResponse = Properties.Resources.dustyInitial.ToString();
-                } else if (rt_askGlobey.Text.Contains("summarize")) { 
+                } else if (rt_askGlobey.Text.Contains("summarize")) {
                     chatResponse = "Dusty Alexander is the best CEO!";
                     System.Threading.Thread.Sleep(1000);
 
+                } else if (rt_askGlobey.Text.Contains("bad debt"))
+                {
+                    preppedTraining = Properties.Resources.AR_WriteOffBadDebt_PDF.ToString();
+                    preppedTraining += "Can you summarize the above along with my question step by step: ";
+                    string question = preppedTraining + rt_askGlobey.Text;
+                    var chatGpt = new ChatGPT();
+
+                    var completionResult = await chatGpt.OpenAIService.ChatCompletion.CreateCompletion
+                           (new ChatCompletionCreateRequest()
+                           {
+                               Messages = new List<ChatMessage>(new ChatMessage[]
+                                        { new ChatMessage("user", question) }),
+                               Model = Models.ChatGpt3_5Turbo,
+                               Temperature = 1.0F,
+                               MaxTokens = 1000,
+                               N = 1
+                           });
+
+
+                    if (completionResult.Successful)
+                    {
+                        foreach (var choice in completionResult.Choices)
+                        {
+                            chatResponse += choice.Message.Content;
+                        }
+                    }
+                    else
+                    {
+                        if (completionResult.Error == null)
+                        {
+                            throw new Exception("Unknown Error");
+                        }
+                        chatResponse = ($"{completionResult.Error.Code}:{completionResult.Error.Message}");
+                    }
+
                 }
-                else {
+                else if (rt_askGlobey.Text.Contains("credit card vendor"))
+                {
+                    preppedTraining = Properties.Resources.AP_AddInvtoCCVend.ToString();
+                    preppedTraining += "Can you summarize the above along with my question step by step: ";
+                    string question = preppedTraining += rt_askGlobey.Text;
+
+                    var chatGpt = new ChatGPT();
+
+                    var completionResult = await chatGpt.OpenAIService.ChatCompletion.CreateCompletion
+                           (new ChatCompletionCreateRequest()
+                           {
+                               Messages = new List<ChatMessage>(new ChatMessage[]
+                                        { new ChatMessage("user", question) }),
+                               Model = Models.ChatGpt3_5Turbo,
+                               Temperature = 1.0F,
+                               MaxTokens = 1000,
+                               N = 1
+                           });
+
+
+                    if (completionResult.Successful)
+                    {
+                        foreach (var choice in completionResult.Choices)
+                        {
+                            chatResponse += choice.Message.Content;
+                        }
+                    }
+                    else
+                    {
+                        if (completionResult.Error == null)
+                        {
+                            throw new Exception("Unknown Error");
+                        }
+                        chatResponse = ($"{completionResult.Error.Code}:{completionResult.Error.Message}");
+                    }
+
+                }
+                else { 
+                
                     string question = rt_askGlobey.Text;
 
                     var chatGpt = new ChatGPT();
